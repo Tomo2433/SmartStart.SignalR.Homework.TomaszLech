a91368a24f656d012634719b854aa34f595bc07b
Tomo2433 <tomaszlech56@gmail.com>
2022-12-20 17:12:59 +0100

    Add project files.

---
+++ SignalrChatServer/ConsoleSignalrClientChat/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace ConsoleSignalrClientChat
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var hubConnection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5257/hubs/chat")
+                .ConfigureLogging(x =>
+                {
+                    x.AddConsole();
+                    x.SetMinimumLevel(LogLevel.Error);
+                })
+                .Build();
+            
+            hubConnection.On<string,string>("showString", ShowMessage);
+            await hubConnection.StartAsync();
+
+            Console.WriteLine("Write username: ");
+            var username = Console.ReadLine();
+            bool flag = true;
+            while (flag) 
+            { 
+                Console.WriteLine("Write message (or type \"/exit\" to leave chat): ");
+                var message = Console.ReadLine();
+                if (message == "/exit")
+                {
+                    flag = false;
+                    break;
+                }
+                await hubConnection.InvokeAsync("SendMessage", username, message);
+            }
+        }
+
+        private static void ShowMessage(string username, string message)
+        {
+            Console.WriteLine($"<{username}>: {message}");
+        }
+    }
+}
---
+++ SignalrChatServer/SignalrChatServer/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalrChatServer.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string username, string message)
+        {
+            await Clients.All.SendAsync("showString", username, message);
+        }
+    }
+}
